38c8a783983cad80d57c0fe4c6caffa56fa7ee14
Yuan25 <chuahlayyuan@gmail.com>
2018-11-06 15:32:40 +0800

    Initial commit

---
+++ app/src/main/java/justcodeenterprise/com/a03_learnfragment_alltherages/MainActivity.kt
@@ -0,0 +1,12 @@
+package justcodeenterprise.com.a03_learnfragment_alltherages
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
